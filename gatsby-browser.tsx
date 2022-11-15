1470272e6d9ba2ce17129cb25793708c46d5c492
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2022-11-15 18:16:03 +0400

    Update Project info

---
+++ gatsby-browser.tsx
@@ -0,0 +1,2 @@
+import './src/styles.css';
+import 'prismjs/themes/prism-coy.css';
--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,41 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Blog`,
+    title: `GRGN`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
-}
+  plugins: [
+    'gatsby-plugin-mdx',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-rss-feed`,
+      options: {
+        url: `https://xn--69aa8bzb.xn--y9a3aq/@gurgen.rss`,
+        name: `Mastodon`,
+      },
+    },
+  ],
+};
 
-export default config
+export default config;
